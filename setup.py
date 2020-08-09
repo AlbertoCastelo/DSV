8eb968656a46cbf816c54033ce53e7e220f11361
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2020-08-09 20:05:16 +0200

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dsv", 
+    name="dsv-cli",
     version="0.0.1",
     author="Alberto Castelo",
     author_email="alberto.castelo.becerra@gmail.com",
